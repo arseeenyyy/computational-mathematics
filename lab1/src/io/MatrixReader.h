5804c8cddfec6badb640c963e03953c1471d5b73
arseeenyyy <rubtsov113@mail.ru>
2025-02-20 18:34:38 +0300

    prepare structure lab1

---
+++ lab1/src/io/MatrixReader.h
@@ -0,0 +1,15 @@
+#ifndef MATRIX_READER_H
+#define MATRIX_READER_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class MatrixReader {
+public: 
+    static vector<vector<double>> readFromFile(const string &filename);
+    static vector<vector<double>> readFromKeyboard(int &n);
+};
+
+#endif
