32068ed5567d61908bc0c9e50df4341a157b1574
arseeenyyy <rubtsov113@mail.ru>
2025-02-21 17:26:05 +0300

    add pretty print

--- lab1/src/core/GaussSeidel.cpp
+++ lab1/src/core/GaussSeidel.cpp
@@ -3,45 +3,55 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <iomanip>  // Для форматированного вывода
 
-using namespace std; 
+using namespace std;
 
 void GaussSeidel::gaussSeidelMethod(vector<vector<double>> &matrix, vector<double> &x, double accuracy) {
     int n = matrix.size();
     int iteration = 0;
-
     for (int i = 0; i < n; i++) {
         x[i] = matrix[i][n] / matrix[i][i];
     }
+
+    cout << setw(7) << "№";
+    for (int i = 0; i < n; i++) {
+        cout << setw(10) << "x" + to_string(i + 1);  
+    }
+    for (int i = 0; i < n; i++) {
+        cout << setw(1) << "ε" + to_string(i + 1);  
+    }
+    cout << endl;
+
     
     while (true) {
         vector<double> new_x = x;  
-        vector<double> error(n, 0); 
+        vector<double> epsilon(n, 0);
 
-        
         for (int i = 0; i < n; i++) {
             double sum = 0;
             for (int j = 0; j < n; j++) {
                 if (i != j) sum += matrix[i][j] * new_x[j];
             }
             new_x[i] = (matrix[i][n] - sum) / matrix[i][i];
-            error[i] = abs(new_x[i] - x[i]);
-        }
-        cout << "Iteration " << iteration + 1 << " - Values of x: ";
-        for (int i = 0; i < n; i++) {
-            cout << new_x[i] << " ";
+            epsilon[i] = abs(new_x[i] - x[i]);
         }
 
-        cout << "| Errors: ";
+        cout << setw(7) << iteration + 1; 
         for (int i = 0; i < n; i++) {
-            cout << error[i] << " ";
+            cout << setw(10) << fixed << setprecision(4) << new_x[i];  
+        }
+        for (int i = 0; i < n; i++) {
+            cout << setw(10) << fixed << setprecision(4) << epsilon[i]; 
         }
         cout << endl;
-        double max_error = *max_element(error.begin(), error.end());
+
+        double max_error = *max_element(epsilon.begin(), epsilon.end());
         if (max_error <= accuracy) {
             break;  
         }
         x = new_x;
         iteration++;
     }
+    cout << "\nMethod converged in " << iteration + 1 << " iterations.\n";
 }
