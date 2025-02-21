2ed6e30d338d60b13854516344e1a595ed6eb6c3
arseeenyyy <rubtsov113@mail.ru>
2025-02-21 14:50:33 +0300

    some extra checks

--- lab1/src/io/MatrixReader.cpp
+++ lab1/src/io/MatrixReader.cpp
@@ -17,6 +17,10 @@ vector<vector<double>> MatrixReader :: readFromFile(const string &filename, int 
         return {};
     }
     file >> accuracy;
+    if (accuracy == 0) {
+        cerr << "Incorrect accuracy!\n";
+        return {};
+    }
     vector<vector<double>> matrix(n, vector<double>(n + 1));
     for (int i = 0; i < n; i ++) {
         for (int j = 0; j < n + 1; j ++) {
@@ -27,14 +31,24 @@ vector<vector<double>> MatrixReader :: readFromFile(const string &filename, int 
     return matrix;
 }
 vector<vector<double>> MatrixReader :: readFromKeyboard(int &n, double &accuracy) {
-    cout << "Enter matrix size(n <= 20): "; 
-    cin >> n;
-    if (n <= 0 || n > 20) {
-        cerr << "Incorrect matrix size!\n";
-        return {};
+    while (true) {
+        cout << "Enter matrix size (n <= 20): "; 
+        cin >> n;
+        if (n > 0 && n <= 20) {
+            break; 
+        } else {
+            cerr << "Incorrect matrix size! Please enter a value between 1 and 20.\n";
+        }
     }
-    cout << "Enter accuracy (double): ";
-    cin >> accuracy;
+    while (true) {
+        cout << "Enter accuracy (double): ";
+        cin >> accuracy;
+        if (accuracy > 0) {
+            break;  
+        } else {
+            cerr << "Accuracy must be positive. Please enter a positive value.\n";
+        }
+    }
     cout << "Enter matrix params line by line\n";
     vector<vector<double>> matrix(n, vector<double>(n + 1)); 
     for (int i = 0; i < n; i ++) {
