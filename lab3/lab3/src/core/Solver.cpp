9667da8c819e38296db07bdb98b69197ca9ab6d6
arseeenyyy <rubtsov113@mail.ru>
2025-04-20 23:04:16 +0300

    done with rectangle methods

--- lab3/lab3/src/core/Solver.cpp
+++ lab3/lab3/src/core/Solver.cpp
@@ -1,22 +1,39 @@
 #include "Solver.h"
+#define INITIAL_N 4
 
 
 double left_rectangle(double (*f)(double), std::pair<double, double> limits, double accuracy) {
     double result = 0;
-    int initial_n = 4;
-    double h = (limits.second - limits.first) / initial_n;
-    for (int i = 0; i < initial_n; i ++) {
+    int n = INITIAL_N;
+    double h = (limits.second - limits.first) / n;
+    for (int i = 0; i < n; i ++) {
         result += f(limits.first + i * h);
     }
+    result *= h;
     return result;
-
 }
 double right_rectangle(double (*f)(double), std::pair<double, double> limits,  double accuracy) {
-    return 1;
+    double result = 0;
+    int n = INITIAL_N;
+    double h = (limits.second - limits.first) / n;
+    for (int i = 0 ; i < n + 1; i ++) {
+        result += f(limits.first + i * h);
+    }
+    result *= h;
+    return result;
 }
 double mid_rectangle(double (*f)(double), std::pair<double, double> limits,  double accuracy) {
-    return 1;
+    double result = 0;
+    int n = INITIAL_N;
+    double h = (limits.second - limits.first) / n;
+    for (int i = 0; i < n; i ++) {
+        result += f(limits.first + (i + 0.5) * h);
+    }
+    result *= h;
+    return result;
 }
+
+
 double trapezoidal(double (*f)(double), std::pair<double, double> limits,  double accuracy) {
     return 1;
 }
