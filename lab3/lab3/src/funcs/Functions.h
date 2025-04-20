6117c7ee5fe780d535474b9a9dbb69ef85678ebe
arseeenyyy <rubtsov113@mail.ru>
2025-04-20 20:37:46 +0300

    make function prettier

--- lab3/lab3/src/funcs/Functions.h
+++ lab3/lab3/src/funcs/Functions.h
@@ -2,25 +2,30 @@
 #define FUNCTIONS_H
 
 #include <cmath>
+#include <string>
+#include <array>
 
-inline double f1(double x) {
-    return x * x;  
+using MathFunction = double(double);
+
+struct FunctionInfo {
+    MathFunction* function;   
+    std::string name;         
+};
+
+inline double f1(double x) { return x * x; }
+inline double f2(double x) { return std::exp(x); }
+inline double f3(double x) { return 1.0 / x; }
+inline double f4(double x) { 
+    return -3.0 * x * x * x - 5.0 * x * x + 4.0 * x - 2.0; 
 }
+inline double f5(double x) { return 5.0; }
 
-inline double f2(double x) {
-    return std::exp(x);
-}
+inline std::array<FunctionInfo, 5> AVAILABLE_FUNCTIONS = {{
+    {&f1, "x^2"},
+    {&f2, "e^x"},
+    {&f3, "1/x"},
+    {&f4, "-3x³-5x²+4x-2"},
+    {&f5, "5"}
+}};
 
-inline double f3(double x) {
-    return 1.0 / x;
-}
-
-inline double f4(double x) {
-    return -3.0 * x * x * x - 5.0 * x * x + 4.0 * x - 2.0;
-}
-
-inline double f5(double x) {
-    return 5.0;
-}
-
-#endif
+#endif 
--- lab3/lab3/src/main.cpp
+++ lab3/lab3/src/main.cpp
@@ -1,10 +1,14 @@
+#include <cstddef>
 #include <iostream>
+#include "funcs/Functions.h"
 
 using namespace std;
 
 
 int main() {
-    cout << "hello";
-
+    for (size_t i = 0 ; i < AVAILABLE_FUNCTIONS.size() ; i ++) {
+        const auto& func = AVAILABLE_FUNCTIONS[i];
+        cout << func.name << "\n" << AVAILABLE_FUNCTIONS[i].function(1)<<"\n\n";
+    }
     return 0;
 }
