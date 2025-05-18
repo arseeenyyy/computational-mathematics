38ee3facec751aa8a48809048dcba70a03bdc995
arseeenyyy <rubtsov113@mail.ru>
2025-05-18 22:41:09 +0300

    improved euler method done

--- lab6/src/solver.py
+++ lab6/src/solver.py
@@ -8,12 +8,24 @@ def euler_method(f, xi, y0):
         yi.append(y_next)
     return yi
 
+def improved_euler_method(f, xi, y0):
+    yi = [y0]
+    h = xi[1] - xi[0]
+    for i in range(len(xi)):
+        y_pred = f(xi[i], yi[i])
+        y_corr = f(xi[i] + h, yi[i] + h * y_pred)
+        yi.append(yi[i] + 0.5 * h * (y_pred + y_corr))
+    return yi
+
+
 
 def main(): 
     f = lambda x, y: x + y
     xi = np.arange(0, 1.0, 0.1).tolist()
     y0 = 1
     yi = euler_method(f, xi, y0);
+    print(yi)
+    yi = improved_euler_method(f, xi, y0)
     print(yi)
 
 if __name__ == "__main__": 
